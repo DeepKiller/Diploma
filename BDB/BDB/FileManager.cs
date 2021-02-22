a897940b389f201357c8fb430f97cf6e7c5f9aad
DeepKiller <deep.killer@yandex.by>
2021-02-22 16:40:54 +0300

    Запись в файл V1

--- BDB/BDB/FileManager.cs
+++ BDB/BDB/FileManager.cs
@@ -5,10 +5,10 @@ namespace BDB
 {
     class Table
     {
-        public string name;// название таблицы
-        private Row[] rows; // перечень рядов, 0 - заголовок
+        public string Name;// название таблицы
+        private Row[] Rows; // перечень рядов, 0 - заголовок
         private string[] Ids; // Ключи записей и их позиции
-        private string path; // расположение файла таблицы 
+        private string Path; // расположение файла таблицы 
         
         public Table()
         {
@@ -24,47 +24,78 @@ namespace BDB
             else
             {
                 string[] tPath = FilePath.Split('\\');
-                path = FilePath;
-                name = tPath[tPath.Length-1].Split('.')[0];
+                Path = FilePath;
+                Name = tPath[tPath.Length-1].Split('.')[0];
             }
             MakeForConstruct();
         }
         private void GenerateNewFile()
         {
             string[] Files = Directory.GetFiles(Directory.GetCurrentDirectory(), "Table*.bdbt");
-            name = "Table" + (Files.Length + 1);
-            path = name + ".bdbt";
+            Name = "Table" + (Files.Length + 1);
+            Path = Name + ".bdbt";
         }
         private void MakeForConstruct()
         {
-            File.Create(path);
-            rows = new Row[0];
+            File.Create(Path).Close();
+            Rows = new Row[0];
             Ids = new string[0];
         }
         public void SetColNames(string[] ColNames) 
         {
-            if(rows.Length == 0)
-                Array.Resize(ref rows, 1);
-            string[] CL = { "id"};
+            if(Rows.Length == 0)
+                Array.Resize(ref Rows, 1);
+            string[] CL = { "id|"};
             int pos = 1;
             for (int i = 1; i < ColNames.Length; i++)
             {
                 if (ColNames[i - pos].ToLower() != "id")
                 {
                     Array.Resize(ref CL, CL.Length+1);
-                    CL[i] = ColNames[i - pos];
+                    if (i != ColNames.Length - 1)
+                        CL[i] = ColNames[i - pos] + "|";
+                    else
+                        CL[i] = ColNames[i - pos] + ";";
                 }
                 else
                 {
                     pos--;
                 }
             }
-            rows[0] = new Row(CL);
+            Rows[0] = new Row(CL);
         }
         public void AddRow(string[] RowData)
         {
-            Array.Resize(ref rows, rows.Length + 1);
-            rows[rows.Length] = new Row(RowData);
+            if (RowData.Length > Rows[0].cols.Length - 1)
+                throw new Exception("too many arguments");
+            string[] arrtowrite = new string[RowData.Length + 1];
+            arrtowrite[0] = Rows.Length.ToString();
+            RowData.CopyTo(arrtowrite, 1);
+            for(int i =0; i < arrtowrite.Length - 1; i++)
+            {
+                arrtowrite[i] += "|";
+            }
+            arrtowrite[arrtowrite.Length-1] += ";";
+            Array.Resize(ref Rows, Rows.Length + 1);
+            Rows[Rows.Length-1] = new Row(arrtowrite);
+            Array.Resize(ref Ids, Ids.Length + 1);
+            Ids[Ids.Length - 1] = arrtowrite[0]; 
+        }
+        public void SaveChanges()
+        {
+            File.Delete(Path);
+            StreamWriter sWriter = File.AppendText(Path);
+            for(int i=0; i < Rows.Length; i++)
+            {
+                string write = "";
+                foreach (string data in Rows[i].cols)
+                    write += data;
+                sWriter.WriteLine(write);
+                if (i > 0)
+                    Ids[i - 1] += i;
+            }
+            sWriter.Close();
+            File.AppendAllLines(Path,Ids);
         }
     }
     class Row
--- BDB/BDB/Program.cs
+++ BDB/BDB/Program.cs
@@ -8,7 +8,10 @@ namespace BDB
         {
             Table test = new Table("test.bdbt");
             string[] Cols = { "one", "two", "id" };
+            string[] Data = { "1", "2"};
             test.SetColNames(Cols);
+            test.AddRow(Data);
+            test.SaveChanges();
         }
     }
 }
