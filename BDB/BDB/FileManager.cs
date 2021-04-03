e47ddcb9159a754c60b8d6fc7a001f419d61e2e2
DeepKiller <deep.killer@yandex.by>
2021-04-03 09:06:32 +0300

    Сборка базы, сжатие (НЕ РАБОТАЕТ!!!)

--- BDB/BDB/FileManager.cs
+++ BDB/BDB/FileManager.cs
@@ -5,12 +5,102 @@ using System.Collections;
 
 namespace BDB
 {
-    class DataBase // Управление общим файлом, сборка, шифрование, сжатие, и обратно
+    static class DataBase // Управление общим файлом, сборка, шифрование, сжатие, и обратно
     {
+        private struct CompressData 
+        { 
+            public int Size { get; set; }
+            public int Char { get; set; }
+            public CompressData(int size, byte chr)
+            {
+                Size = size;
+                Char = Convert.ToInt32(chr);
+            }
+        }
+        static string Path = ""; //путь к файлу базы
+        static public void MakeBaseFile(string path)//сборка таблиц в файл базы
+        {
+            ArrayList tables = new ArrayList();
+            foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory(), "*.bdbt"))
+            {
+                Table tab = new Table();
+                tab.LoadTableData(file);
+                tables.Add(tab);
+            }
+            if (File.Exists(path))
+                File.Delete(path);
+            JsonSerializerOptions jsonSerializer = new JsonSerializerOptions();
+            jsonSerializer.WriteIndented = true;
+            string json = JsonSerializer.Serialize(tables, jsonSerializer);
+            File.AppendAllText(path, json);
+            foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory(), "*.bdbt"))
+                File.Delete(file);
+            Path = path;
+        }
+        static public void CompressByGlobalPath(string Password)//сжатие и шифрование базы
+        {
+            byte[] Data = File.ReadAllBytes(Path);
+            int pos = 0;
+            ArrayList compressed = new ArrayList();
+            while (pos < Data.Length)
+            {
+                int Spos = pos;
 
+                while(Data[pos] == Data[Spos])
+                {
+                    Spos++;
+                    if (Spos == Data.Length)
+                        break;
+                }
+                compressed.Add(new CompressData(Spos - pos, Data[pos]));
+                pos = Spos;
+            }
+            File.Delete(Path);
+            Data = new byte[compressed.Count*2];
+            pos = 0;
+            for(int i=0; i < Data.Length; i += 2)
+            {
+                Data[i] = Convert.ToByte(((CompressData)compressed[pos]).Size);
+                Data[i + 1] = Convert.ToByte(((CompressData)compressed[pos]).Char);
+                pos++;
+            }
+            File.WriteAllBytes(Path,Data);
+        }
+        static public void DecompressByGlobalPath(string Password)//востановление базы
+        {
+            byte[] Data = File.ReadAllBytes(Path);
+            byte[] decompressed = new byte[0];
+            for(int i=0; i< Data.Length; i += 2)
+            {
+                for (int j = 0; j < Convert.ToInt32(Data[i]); j++)
+                {
+                    Array.Resize(ref decompressed, decompressed.Length + 1);
+                    decompressed[decompressed.Length - 1] = Data[i + 1];
+                }
+            }
+            File.Delete(Path);
+            File.WriteAllBytes(Path,decompressed);
+        }
     }
+    //TODO: добавть связи таблиц
     class Table //управление таблицами
     {
+        class Row //подкласс для рядов
+        {
+            public ArrayList cols { get; set; } //содержимое строки, столбцы
+            public Row()// НЕ ТРОГАТЬ !!!!!!!!!!!!
+            {
+
+            }
+            public Row(string[] data) //запись данных в объект
+            {
+                cols = new ArrayList();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    cols.Add(data[i]);
+                }
+            }
+        }
         public string Name { set; get; }// название таблицы
         public ArrayList Rows { set; get; }  // перечень рядов, 0 - заголовок
         public ArrayList Ids { set; get; } // Ключи записей и их позиции
@@ -88,6 +178,8 @@ namespace BDB
         {
             string ReadedJson = File.ReadAllText(FilePath);
             Table temp = (Table)JsonSerializer.Deserialize(ReadedJson,GetType());
+            Name = temp.Name;
+            Path = temp.Path;
             for (int i = 0; i < temp.Rows.Count; i++)
             {
                 Row r = (Row)JsonSerializer.Deserialize(((JsonElement)temp.Rows[i]).ToString(), new Row().GetType());
@@ -109,20 +201,5 @@ namespace BDB
             File.Delete(Path);
         }
     }
-    class Row //подкласс для рядов
-    {
-        public ArrayList cols { get; set; } //содержимое строки, столбцы
-        public Row()// НЕ ТРОГАТЬ !!!!!!!!!!!!
-        {
 
-        }
-        public Row(string[] data) //запись данных в объект
-        {
-            cols = new ArrayList();
-            for(int i = 0; i < data.Length; i++)
-            {
-                cols.Add(data[i]);
-            }
-        }
-    }
 }
--- BDB/BDB/Program.cs
+++ BDB/BDB/Program.cs
@@ -11,8 +11,13 @@
             test.AddRow(Data);
             test.SaveChanges();
             Table test1 = new Table("test1.bdbt");
-            test1.LoadTableData("test.bdbt");
+            test1.SetColNames(Cols);
+            test1.AddRow(Data);
+            test.LoadTableData("test.bdbt");
             test1.SaveChanges();
+            DataBase.MakeBaseFile("test.bdb");
+            DataBase.CompressByGlobalPath("Pass");
+            //DataBase.DecompressByGlobalPath("Pass");
         }
     }
 }
